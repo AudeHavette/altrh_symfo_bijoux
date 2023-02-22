f4fa48d820d5ce928a2cf23b5e909d156b67ff0f
Aude Havette <aude.ma@hotmail.fr>
2023-02-22 17:06:57 +0100

    commit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -100,17 +101,46 @@ class ProductController extends AbstractController
  * @Route("/list", name="listProduct")
  */
 
-public function listProduct()
+public function listProduct(ProductRepository $productRepository)
 {
-return $this->render('product/listProduct.html.twig',[
+    //le repository permetd'effectuer les requetes de SELECT, sa meth findAll() équivaut à SELECT*FROM nomdeLentite; 
+    $products=$productRepository->findAll();
+    //dd($products); //$products contient tte les entrees de la T product en BDD
 
+
+
+
+return $this->render('product/listProduct.html.twig',[
+'products'=>$products
 ]);
 
 }
 
+/**
+ * 
+ * @Route("/edit/{id}", name="editProduct")
+ */
 
+ public function editProduct()
 
+ {
+    return $this->render('product/editProduct.html.twig', [
 
+    ]);
+
+ }
+
+ /**
+ * 
+ * @Route("/delete/{id}", name="deleteProduct")
+ */
+
+ public function deleteProduct()
+
+ {
+    return $this->redirectToRoute('listProduct') ;
+
+ }
 
 
 
